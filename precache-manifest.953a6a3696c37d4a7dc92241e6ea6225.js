51e8c7d4837d5718af2a939c36f2bc792e337b36
nikhil.mate <nikhil.mate@media.net>
2019-11-30 12:54:32 +0530

    Updates

---
+++ precache-manifest.953a6a3696c37d4a7dc92241e6ea6225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38be6ccd63ad6a23a8f08b8df9233de3",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "4eb345355510861fb768",
+    "url": "/starwars/static/css/main.8bdc802d.chunk.css"
+  },
+  {
+    "revision": "8e8a6337e920a94d36e0",
+    "url": "/starwars/static/js/2.7926aec9.chunk.js"
+  },
+  {
+    "revision": "4eb345355510861fb768",
+    "url": "/starwars/static/js/main.9cc7f012.chunk.js"
+  },
+  {
+    "revision": "e2b2f144e42e097a48e3",
+    "url": "/starwars/static/js/runtime-main.9492929c.js"
+  }
+]);
